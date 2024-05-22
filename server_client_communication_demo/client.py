0900490358e2578cd2d6210a5622a5a5e5a45878
M. Shehroz Wali Khan <mswkhan27@gmail.com>
2024-05-22 16:04:07 +0300

    Loaded FMU in Server

--- server_client_communication_demo/client.py
+++ server_client_communication_demo/client.py
@@ -14,20 +14,28 @@ async def main():
         nsidx = await client.get_namespace_index(namespace)
         print(f"Namespace Index for '{namespace}': {nsidx}")
 
-        # Get the variable node for read / write
-        var = await client.nodes.root.get_child(
-            f"0:Objects/{nsidx}:MyObject/{nsidx}:MyVariable"
-        )
-        value = await var.read_value()
-        print(f"Value of MyVariable ({var}): {value}")
+        #Values from FMU
+        var_names=['T1_in', 'm1_in', 'T2_in', 'm2_in', 'T2_out', 'm2_out', 'T1_out', 'm1_out', 'K', 'T1_ini', 'T2_ini', 'cp1', 'cp2', 'm1', 'm2', 'time']
+        
+        for var_name in var_names:
+            # Get the variable node for read / write
+            var = await client.nodes.root.get_child(
+                f"0:Objects/{nsidx}:MyObject/{nsidx}:{var_name}"
+            )
+            value = await var.read_value()
+            print(f"Value of {var_name} ({var}): {value}")
 
-        new_value = value - 50
-        print(f"Setting value of MyVariable to {new_value} ...")
-        await var.write_value(new_value)
+
+        
+
+
+        # new_value = value - 50
+        # print(f"Setting value of MyVariable to {new_value} ...")
+        # await var.write_value(new_value)
 
         # Calling a method
-        res = await client.nodes.objects.call_method(f"{nsidx}:ServerMethod", 5)
-        print(f"Calling ServerMethod returned {res}")
+        # res = await client.nodes.objects.call_method(f"{nsidx}:ServerMethod", 5)
+        # print(f"Calling ServerMethod returned {res}")
 
 
 if __name__ == "__main__":
--- server_client_communication_demo/server.py
+++ server_client_communication_demo/server.py
@@ -5,18 +5,46 @@ from asyncua import Server, ua
 from asyncua.common.methods import uamethod
 
 
+import pyfmi
+import numpy as np
+from matplotlib import pyplot as plt
+
 @uamethod
 def func(parent, value):
     return value * 2
 
 
+
+@uamethod
+def func(parent, T1_ini,T2_ini):
+    return T1_ini,T2_ini
+
+
+def get_variable_names_from_fmu(fmu):
+    model_variables = fmu.get_model_variables()
+    print(model_variables)
+    variable_names = fmu.get_model_variables().keys()
+    return variable_names
+
+    
+
+
 async def main():
     _logger = logging.getLogger(__name__)
+    #Load FMUs
+    pth = "../fmus/"
+    fnm = "hex_delta.fmu"
+    fmu = pyfmi.load_fmu(pth+fnm)
+
+
+
     # setup our server
     server = Server()
     await server.init()
     server.set_endpoint("opc.tcp://0.0.0.0:4840/freeopcua/server/")
 
+    
+    
     # set up our own namespace, not really necessary but should as spec
     uri = "http://examples.freeopcua.github.io"
     idx = await server.register_namespace(uri)
@@ -24,9 +52,21 @@ async def main():
     # populating our address space
     # server.nodes, contains links to very common nodes like objects and root
     myobj = await server.nodes.objects.add_object(idx, "MyObject")
-    myvar = await myobj.add_variable(idx, "MyVariable", 6.7)
-    # Set MyVariable to be writable by clients
-    await myvar.set_writable()
+    t1_ini = await myobj.add_variable(idx, "T1_ini,", '')
+    t2_ini = await myobj.add_variable(idx, "T2_ini", '')
+
+    #Putting Variables from FMU to OPC Variables and making that writable for Client side
+
+    fmu_variables=get_variable_names_from_fmu(fmu)
+    opc_variables=[]
+    for v in fmu_variables:
+
+        opc_var=await myobj.add_variable(idx, v, float(fmu.get(v)))
+        opc_variables.append(opc_var)
+        # Set MyVariable to be writable by clients
+        await opc_var.set_writable()
+
+        
     await server.nodes.objects.add_method(
         ua.NodeId("ServerMethod", idx),
         ua.QualifiedName("ServerMethod", idx),
@@ -38,9 +78,12 @@ async def main():
     async with server:
         while True:
             await asyncio.sleep(1)
-            new_val = await myvar.get_value() + 0.1
-            _logger.info("Set value of %s to %.1f", myvar, new_val)
-            await myvar.write_value(new_val)
+            for variable_name_from_opc in opc_variables:
+                fmu_value_stored_in_opc = await variable_name_from_opc.get_value()
+                value = fmu_value_stored_in_opc
+                print(variable_name_from_opc.get_variables(),value)
+            # _logger.info("Set value of %s to %.1f", myvar, new_val)
+            # await myvar.write_value(new_val)
 
 
 if __name__ == "__main__":

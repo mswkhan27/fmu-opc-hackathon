a00ccd202e672e980fb929d5b215d7887da7d06b
Mikael Manngård <mikman2@novia.fi>
2024-05-22 13:30:41 +0300

    Simulink HE model

---
+++ fmus/hx_parameters.m
@@ -0,0 +1,14 @@
+%% general parameters
+A = 380; %total surface area [m^2]
+h = 6800; %heat transfer coefficient 1 [W/m^2/K]
+K = h*A; %heat-transfer parameters
+%% side 1
+T1_ini = 68; %Initial temperature [degC]
+m1 = 400; %liquid mass [kg]
+cp1 = 4190; %specific heat capacity side 1 [J/kg/K]
+
+
+%% side 2
+T2_ini = 91; %Initial temperature [degC]
+m2 = 400; %liquid mass [kg]
+cp2 = 4190; %specific heat capacity side 2 [J/kg/K]
